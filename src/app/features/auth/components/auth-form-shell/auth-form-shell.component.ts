a79d862ff6b70fec5059fb02fd5a6af23f46e006
MuhamedElshikh <mhamedelshikh3@gmail.com>
2026-08-02 21:15:43 +0300

    gaSss

--- src/app/features/auth/components/auth-form-shell/auth-form-shell.component.ts
+++ src/app/features/auth/components/auth-form-shell/auth-form-shell.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { NavbarComponent } from "../../../../layout/components/navbar/navbar.component";
 
 /** Shared shell for auth screens. */
 @Component({
   selector: 'app-auth-form-shell',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, NavbarComponent],
   standalone: true,
   templateUrl: './auth-form-shell.component.html',
   styleUrl: './auth-form-shell.component.css'
