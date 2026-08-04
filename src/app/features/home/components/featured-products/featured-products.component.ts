9fb1a962b30a21db1c15556850514e0c8f1d834b
MuhamedElshikh <mhamedelshikh3@gmail.com>
2026-08-04 11:34:52 +0300

    ad

--- src/app/features/home/components/featured-products/featured-products.component.ts
+++ src/app/features/home/components/featured-products/featured-products.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, inject, signal,ElementRef, ViewChild,AfterViewInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CatalogService } from '../../../../core/services/catalog.service'; // عدّل المسار
 import { ProductCardResponse } from '../../../../core/models/catalog.models'; // عدّل المسار
 import { ProductCardComponent } from '../../../catalog/components/product-card/product-card.component'; // عدّل المسار
 import { TranslatePipe } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-featured-products',
@@ -12,11 +13,14 @@ import { TranslatePipe } from '@ngx-translate/core';
   templateUrl: './featured-products.component.html',
   styleUrl: './featured-products.component.css',
 })
-export class FeaturedProductsComponent implements OnInit {
+export class FeaturedProductsComponent implements OnInit , AfterViewInit  {
   private catalog = inject(CatalogService);
+  @ViewChild('sliderTrack') sliderTrack?: ElementRef<HTMLDivElement>;
 
   products = signal<ProductCardResponse[]>([]);
   loading = signal(true);
+   canScrollPrev = signal(false);
+  canScrollNext = signal(true);
 
   ngOnInit(): void {
     this.catalog.getProducts({ pageSize: 8, pageIndex: 1, sortBy: 'newest' }).subscribe({
@@ -26,5 +30,29 @@ export class FeaturedProductsComponent implements OnInit {
       },
       error: () => this.loading.set(false),
     });
+  }
+  ngAfterViewInit() {
+    // نتأكد من حالة الأزرار بعد ما البيانات تتحمل
+    setTimeout(() => this.onSliderScroll(), 0);
+  }
+   scrollSlider(direction: 1 | -1) {
+    const track = this.sliderTrack?.nativeElement;
+    if (!track) return;
+
+    const cardWidth = track.querySelector('.slider-item')?.clientWidth ?? 280;
+    const gap = 16; // نفس الـ gap اللي في الـ SCSS
+    const scrollAmount = (cardWidth + gap) * direction;
+
+    track.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  }
+
+  onSliderScroll() {
+    const track = this.sliderTrack?.nativeElement;
+    if (!track) return;
+
+    this.canScrollPrev.set(track.scrollLeft > 5);
+    this.canScrollNext.set(
+      track.scrollLeft < track.scrollWidth - track.clientWidth - 5
+    );
   }
 }
